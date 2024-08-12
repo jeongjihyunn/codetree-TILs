c344e9c75a2b00d599c234a33b01ccaddcc2e8a9
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-12 20:38:24 +0900

    Title: 두줄 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/두줄 출력/print-two-sentences-introduce.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello students!";
+    cout<<"\n";
+    cout<<"Welcome to LeebrosCode!";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
