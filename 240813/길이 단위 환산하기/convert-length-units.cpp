1391c6f8a2c43fe092aba5622f69de3bd10f0856
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-13 18:54:20 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<fixed;
+    double n;
+    cin>>n;
+    cout.precision(1);
+    cout<<n*30.48;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
