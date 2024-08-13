e68b2704628f2d10c48089362fb660d5c538ba29
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-13 19:00:42 +0900

    Title: 입력받은 값 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,temp;
+    cin >>a>>b;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<a<<" "<<b;// 여기에 코드를 작성해주세요.
+    return 0;
+}
