990cad7a3bb89917042bdfcc9152ac41a5dce828
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-13 21:14:23 +0900

    Title: 삼항연산자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/삼항연산자/ternary-operator.cpp
+++ 240813/삼항연산자/ternary-operator.cpp
@@ -4,14 +4,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int score = n == 100 ? 0 : 1;
-    if(score == 0){
-    cout<<"pass"; 
-    }
-    else{
-        cout<<"failure";
-    }
-
+cout<<((n==100)?"pass":"failure");
     // 여기에 코드를 작성해주세요.
     return 0;
 }
