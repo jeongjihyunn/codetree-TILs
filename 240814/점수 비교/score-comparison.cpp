2e5749f90705bcffcff0638a87e3a7dc6db9f5ab
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-14 01:44:44 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240814/점수 비교/score-comparison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,d,e;
+    cin >>a>>b>>d>>e;
+    if(a>d&&b>e){
+        cout<<1;
+    }
+    else{
+        cout<<0;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
