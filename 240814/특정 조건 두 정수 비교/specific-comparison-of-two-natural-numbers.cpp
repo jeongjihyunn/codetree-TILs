59859d74b9210af186dd4fdb1b0aace9134b3fa0
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-14 01:22:11 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240814/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >>x>>y;
+    if(x<y){
+    cout<<1<<" ";
+    }
+    else{
+        cout<<0<<" ";
+    }
+    if(x==y){
+        cout<<1;
+    }
+    else{
+        cout<<0;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
