762acd76506bdd6d366df14258bad1a3e59b5ca5
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-13 20:44:58 +0900

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240813/체질량지수/bmi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int h,w,b;
+    cin >>h>>w;
+    b=(10000*w)/(h*h);
+    if(b>=25){
+        cout<<b<<endl<<"Obesity";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
