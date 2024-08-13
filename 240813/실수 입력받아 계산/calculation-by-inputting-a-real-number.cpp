cc60b60beabe31d5a452a6e6a93a1a291b169c91
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-13 19:09:03 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<fixed;
+    double a,b;
+    cin>>a>>b;
+cout.precision(2);
+cout<<a+b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
