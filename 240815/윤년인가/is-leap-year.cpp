f9875ea918bb2bb701cbb7d4c9e1b622aebe6677
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-15 14:13:23 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240815/윤년인가/is-leap-year.cpp
+++ 240815/윤년인가/is-leap-year.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main() {
     int y;
     cin >>y;
-    if((y%4==0)&&(y%100!=0)){
-       cout<<"true";
-    }
-    else if(y%400!=0){
+    if((y%400==0)||(y%100!=0)&&(y%4==0)){
+        cout<<"true";}
+    else{
         cout<<"false";
     }
+    
+    
     // 여기에 코드를 작성해주세요.
     return 0;
 }
