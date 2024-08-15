f478b57b1a4d305cb1af106e09ac0f52f1af64fe
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-15 16:11:47 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240815/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,i=1;
+    cin >>n;
+    while(i<=n){
+        cout<<"*"<<endl;
+        i++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
