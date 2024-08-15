2ea07cf0dc02eaae2624c981de93c2575e1f8302
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-15 13:46:35 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240815/윤년인가/is-leap-year.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y;
+    cin >>y;
+    if((y%100!=0)&&(y%400==0)||y%4==0)
+        {
+            cout<<"true";
+        }
+    else{
+            cout<<"false";
+        }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
