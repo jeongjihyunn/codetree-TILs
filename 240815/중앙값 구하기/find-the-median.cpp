75f8b24a0fe9b19d98939ed169aff763f1d47e68
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-15 15:06:57 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240815/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >>a>>b>>c;
+    if ((a>b&&c>a)||(b>a&&a>c)){
+        cout<<a;
+    }
+    else if((a>b&&b>c)||(c>b&&b>a)){
+        cout<<b;
+    }
+    else{
+        cout<<c;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
