e6c08601f80869c2b24a090df3d5fa0f7c9a501f
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-15 15:45:18 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240815/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,i;
+    cin >>a>>b;
+    for (i=a;i<=b;i+=2){
+            cout<<i<<" ";
+        }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
