8f19076ac0808c31eaa1437300c17b97451302f5
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-15 21:37:53 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240815/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >>b>>a;
+    while(a<=b){
+    cout<<b<<" ";
+    b-=2;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
