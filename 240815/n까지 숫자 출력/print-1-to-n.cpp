7d024757fe2fbd11f4cf8057a1484f80e5002387
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-15 16:09:02 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240815/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int i=1;
+    cin >>n;
+    while(i<=n){
+        cout<<i<<" ";
+        i++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
