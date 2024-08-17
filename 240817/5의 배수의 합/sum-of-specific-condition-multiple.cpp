eb13038b64db549ce4d154db798fce28fafa0052
jeongjihyunn <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-17 20:32:54 +0900

    Title: 5의 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240817/5의 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 240817/5의 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -6,14 +6,14 @@ int main() {
     cin >>a>>b;
     int sum=0;
     if(a>b){
-        for(int i=a;i<=b;i++){
+        for(int i=b;i<=a;i++){
             if(i%5==0){
                 sum +=i;
             }
         }
     }
     else if(a<=b){
-        for(int i=b;i<=a;i++){
+        for(int i=a;i<=b;i++){
             if(i%5==0){
                 sum +=i;
             }
