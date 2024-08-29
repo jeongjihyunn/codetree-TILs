c2094cf63f1c11e43a2b97dbf8c177e0957f2584
jeongjihyun <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-30 02:22:19 +0900

    Title: 몫 출력하기, Time: 1098ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

---
+++ 240830/몫 출력하기/print-share.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    while(1){
+        cin >>a;
+        if(a%2!=0){
+        }
+        else if(a%2==0){
+            cout<<a/2<<endl;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
