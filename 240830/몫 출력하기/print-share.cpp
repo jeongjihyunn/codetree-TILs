107da459962abbe8e63b0255f42250bca8a40ec3
jeongjihyun <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-30 02:24:44 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240830/몫 출력하기/print-share.cpp
+++ 240830/몫 출력하기/print-share.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int main() {
     int a;
+    int cnt=0;
     while(1){
         cin >>a;
-        if(a%2!=0){
+        if(a%2==1){
+            continue;
         }
-        else if(a%2==0){
-            cout<<a/2<<endl;
+        cout<<a/2<<endl;
+        cnt++;
+
+        if(cnt>=3){
+            break;
         }
     }
     // 여기에 코드를 작성해주세요.
