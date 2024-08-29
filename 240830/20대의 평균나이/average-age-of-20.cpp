b9534bb2e1a2ad41cea20830febc9cec2bafff99
jeongjihyun <134567893+jeongjihyunn@users.noreply.github.com>
2024-08-30 03:03:24 +0900

    Title: 20대의 평균나이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240830/20대의 평균나이/average-age-of-20.cpp
+++ 240830/20대의 평균나이/average-age-of-20.cpp
@@ -3,20 +3,19 @@ using namespace std;
 
 int main() {
     cout<<fixed;
+    int a;
+    int sum=0,cnt=0;
     while(1){
-        int a;
-        int sum=0,cnt=0;
         cin >>a;
-        if(a>=20 && a<30){
+        if(a<20 || a>=30){
             break;
         }
-        else{
-            sum += a;
-            cnt++;
+        sum += a;
+        cnt++;}
             cout.precision(2);
             cout<<(double)sum/cnt;
-        }
-    }
+        
+    
     // 여기에 코드를 작성해주세요.
     return 0;
 }
