7a5b294278465472d53a9db3ed0cd91f12651aa6
jeongjihyun <134567893+jeongjihyunn@users.noreply.github.com>
2024-09-02 15:21:32 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240902/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt=0;
+    
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            cnt++;
+        }
+    }
+
+    if(cnt==2){
+        cout<<"P";
+    }
+    else{
+        cout<<"C";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
