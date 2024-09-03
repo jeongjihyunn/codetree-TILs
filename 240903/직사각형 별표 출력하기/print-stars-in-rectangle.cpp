189198b358f035c1a9a0af09c5dd848ca9414365
jeongjihyun <134567893+jeongjihyunn@users.noreply.github.com>
2024-09-03 16:47:50 +0900

    Title: 직사각형 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240903/직사각형 별표 출력하기/print-stars-in-rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+
+    for(int i=1;i<=n;i++){
+        for(int i=1;i<=m;i++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
